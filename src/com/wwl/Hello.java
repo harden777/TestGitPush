d3c9ab6f733fa900f06e07ab048d16f4b7d5ca0d
wwl_glb <winln777_glb@163.com>
2021-01-29 19:50:30 +0800

    new brance

--- src/com/wwl/Hello.java
+++ src/com/wwl/Hello.java
@@ -4,5 +4,6 @@ public class Hello {
     public static void main(String[] args) {
         System.out.println("helo Git");
         System.out.println(123);
+        System.out.println("new brance");
     }
 }
