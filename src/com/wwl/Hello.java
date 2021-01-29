3e49a36102fec34f01caab122c380252c4414f00
wwl_glb <winln777_glb@163.com>
2021-01-29 19:56:25 +0800

    confriends

--- src/com/wwl/Hello.java
+++ src/com/wwl/Hello.java
@@ -3,7 +3,7 @@ package com.wwl;
 public class Hello {
     public static void main(String[] args) {
         System.out.println("helo Git");
-        System.out.println(123);
+        System.out.println(12345);
         System.out.println("new brance");
     }
 }
